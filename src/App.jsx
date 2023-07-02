270e817f9dd534512e878a2b36a0716ecc8506ed
ispasdani <ispasdani14@gmail.com>
2023-07-02 22:49:18 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./pages/Home";
+import Customizer from "./pages/Customizer";
+import CanvasModel from "./canvas/intex";
+
+function App() {
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <CanvasModel />
+      <Customizer />
+    </main>
+  );
+}
+
+export default App;
